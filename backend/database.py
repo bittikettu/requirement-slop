b276b00472046d4742fcf98500efc89e9fffbc39
bittikettu <viidakkovekara@gmail.com>
2026-01-11 08:21:12 +0200

    Docker added

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./reqtool.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./reqtool.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
